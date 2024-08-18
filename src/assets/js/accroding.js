9935383adfb30295b6fb40dac7c5bde27a7edf77
tajbirhossain <mdtajbirhossain031@gmail.com>
2024-08-18 13:08:31 +0600

    dksfj

--- src/assets/js/accroding.js
+++ src/assets/js/accroding.js
@@ -28,3 +28,39 @@ accrodionItems.forEach((item, i) => {
         }
     });
 });
+
+
+
+
+
+// const accrodionItems = document.querySelectorAll(".accrodionItem")
+
+
+
+
+// accrodionItems.forEach((item, i) => {
+//     let isOpen = false
+
+//     const accordionHeading = item.querySelector(".accordionHeading")
+//     const accordionDesc = item.querySelector(".accordionDesc")
+//     const accordionOpenSignImg = accordionHeading.querySelectorAll(".accordionOpenSign img")
+
+
+//     item.style = `height: ${accordionHeading.clientHeight + 48}px`
+//     accordionOpenSignImg[0].style = `display: block;`
+//     accordionOpenSignImg[1].style = `display: none;`
+
+//     accordionHeading.addEventListener("click", () => {
+//         if (isOpen) {
+//             item.style = `height: ${accordionHeading.clientHeight + 48}px`
+//             accordionOpenSignImg[0].style = `display: block;`
+//             accordionOpenSignImg[1].style = `display: none;`
+//             isOpen = false
+//         } else {
+//             item.style = `height: ${accordionHeading.clientHeight + accordionDesc.clientHeight + 20 + 48}px`
+//             accordionOpenSignImg[0].style = `display: none;`
+//             accordionOpenSignImg[1].style = `display: block;`
+//             isOpen = true
+//         }
+//     })
+// })
