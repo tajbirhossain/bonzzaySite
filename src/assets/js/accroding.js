68175fe726ba76d47c80b74e06bdce627043dc25
tajbirhossain <mdtajbirhossain031@gmail.com>
2024-08-18 13:07:47 +0600

    kjsdfk

--- src/assets/js/accroding.js
+++ src/assets/js/accroding.js
@@ -1,31 +1,30 @@
-const accrodionItems = document.querySelectorAll(".accrodionItem")
-
-
-
+const accrodionItems = document.querySelectorAll(".accrodionItem");
 
 accrodionItems.forEach((item, i) => {
-    let isOpen = false
+    let isOpen = false;
 
-    const accordionHeading = item.querySelector(".accordionHeading")
-    const accordionDesc = item.querySelector(".accordionDesc")
-    const accordionOpenSignImg = accordionHeading.querySelectorAll(".accordionOpenSign img")
+    const accordionHeading = item.querySelector(".accordionHeading");
+    const accordionDesc = item.querySelector(".accordionDesc");
+    const accordionOpenSignImg = accordionHeading.querySelectorAll(".accordionOpenSign img");
 
+    requestAnimationFrame(() => {
+        item.style.height = `${accordionHeading.clientHeight + 48}px`;
+    });
 
-    item.style = `height: ${accordionHeading.clientHeight + 48}px`
-    accordionOpenSignImg[0].style = `display: block;`
-    accordionOpenSignImg[1].style = `display: none;`
+    accordionOpenSignImg[0].style.display = 'block';
+    accordionOpenSignImg[1].style.display = 'none';
 
     accordionHeading.addEventListener("click", () => {
         if (isOpen) {
-            item.style = `height: ${accordionHeading.clientHeight + 48}px`
-            accordionOpenSignImg[0].style = `display: block;`
-            accordionOpenSignImg[1].style = `display: none;`
-            isOpen = false
+            item.style.height = `${accordionHeading.clientHeight + 48}px`;
+            accordionOpenSignImg[0].style.display = 'block';
+            accordionOpenSignImg[1].style.display = 'none';
+            isOpen = false;
         } else {
-            item.style = `height: ${accordionHeading.clientHeight + accordionDesc.clientHeight + 20 + 48}px`
-            accordionOpenSignImg[0].style = `display: none;`
-            accordionOpenSignImg[1].style = `display: block;`
-            isOpen = true
+            item.style.height = `${accordionHeading.clientHeight + accordionDesc.clientHeight + 20 + 48}px`;
+            accordionOpenSignImg[0].style.display = 'none';
+            accordionOpenSignImg[1].style.display = 'block';
+            isOpen = true;
         }
-    })
-})
+    });
+});
